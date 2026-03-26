46152b3f1fc6a9662a1efab93f7eb8ee11ae91ba
lim <lamanh31122005@gmail.com>
2026-03-26 16:39:44 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
-
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
 createRoot(document.getElementById("root")!).render(
-  <HashRouter> 
+  <BrowserRouter basename="/HRM-system">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
