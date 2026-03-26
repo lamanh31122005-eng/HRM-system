07b770c91fab5f51fc00c2b26394d3e7c3b996fe
lim <lamanh31122005@gmail.com>
2026-03-26 16:26:24 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Đảm bảo đã import cái này
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
-<BrowserRouter basename="/HRM-system">
-  <App />
-</BrowserRouter>
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter basename="/HRM-system">
+    <App />
+  </BrowserRouter>
+);
